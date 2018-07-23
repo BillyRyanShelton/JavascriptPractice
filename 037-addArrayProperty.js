4f44bd4a9c4a03d0115594cde93d305c2378f0f7
BillyRyanShelton <35415026+BillyRyanShelton@users.noreply.github.com>
2018-07-23 14:41:15 -0500

    Create 037-addArrayProperty.js

---
+++ 037-addArrayProperty.js
@@ -0,0 +1,9 @@
+/*Given an object, a key, and an array, “addArrayProperty” sets a new property 
+on the object at the given key, with its value set to the given array.*/
+
+function addArrayProperty(obj, key, arr) {
+  // your code here
+  /* START SOLUTION */
+  obj[key] = arr;
+  /* END SOLUTION */
+}
