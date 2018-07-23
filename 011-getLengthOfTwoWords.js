563c0578e98514e77e9f9ca2643a8ae63c33d3bc
BillyRyanShelton <35415026+BillyRyanShelton@users.noreply.github.com>
2018-07-23 14:21:24 -0500

    Create 011-getLengthOfTwoWords.js

---
+++ 011-getLengthOfTwoWords.js
@@ -0,0 +1,11 @@
+/*Write a function called “getLengthOfTwoWords”.
+Given 2 words, “getLengthOfTwoWords” returns the sum of their lengths.*/
+
+function isGreaterThan10(num) {
+  // your code here
+  /* START SOLUTION */
+  if(num > 10) {
+    return true;
+  } 
+  return false;/* END SOLUTION */
+}
