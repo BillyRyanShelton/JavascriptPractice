8bb4f3af6b0391b6183c96052e27bb6d21af8279
BillyRyanShelton <35415026+BillyRyanShelton@users.noreply.github.com>
2018-07-23 14:18:06 -0500

    Create 007-removeProperty.js

---
+++ 007-removeProperty.js
@@ -0,0 +1,9 @@
+/*Write a function called “removeProperty”.
+Given an object and a key, “removeProperty” removes the given key from the given object.*/
+
+function removeProperty(obj, key) {
+  // your code here
+  /* START SOLUTION */
+  delete obj[key];
+  /* END SOLUTION */
+}
