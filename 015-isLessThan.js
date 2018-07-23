4b1a40232e6ea0ba87dd2f2604d83cc13026da0a
BillyRyanShelton <35415026+BillyRyanShelton@users.noreply.github.com>
2018-07-23 14:24:12 -0500

    Create 015-isLessThan.js

---
+++ 015-isLessThan.js
@@ -0,0 +1,9 @@
+/*Write a function called “isLessThan”.
+Given 2 numbers, “isLessThan” returns whether num2 is less than num1.*/
+
+function isLessThan(num1, num2) {
+  // your code here
+  /* START SOLUTION */
+  return num2 < num1;
+  /* END SOLUTION */
+}
