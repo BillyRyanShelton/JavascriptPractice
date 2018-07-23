998963f29a97af104020facade6d04821ee6654d
BillyRyanShelton <35415026+BillyRyanShelton@users.noreply.github.com>
2018-07-23 14:25:16 -0500

    Create 017-isEqualTo.js

---
+++ 017-isEqualTo.js
@@ -0,0 +1,9 @@
+/*Write a function called “isEqualTo”.
+Given 2 numbers, “isEqualTo” returns whether num2 is equal to num1.*/
+
+function isEqualTo(num1, num2) {
+  // your code here
+  /* START SOLUTION */
+  return num1 === num2;
+  /* END SOLUTION */
+}
