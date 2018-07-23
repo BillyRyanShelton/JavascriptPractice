00961bba2599b8eb7d850d91c492fb062133bd84
BillyRyanShelton <35415026+BillyRyanShelton@users.noreply.github.com>
2018-07-23 14:38:17 -0500

    Create 034-isPersonOldEnoughToDrive.js

---
+++ 034-isPersonOldEnoughToDrive.js
@@ -0,0 +1,9 @@
+/*Given a “person” object, that contains an “age” property, “isPersonOldEnoughToDrive” 
+returns whether the given person is old enough to drive.*/
+
+function isPersonOldEnoughToDrive(person) {
+  // your code here
+  /* START SOLUTION */
+  return person.age > 15;
+  /* END SOLUTION */
+}
