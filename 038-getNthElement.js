fc9bde4a9bc878b9f8a04db6e31365ca4ca92377
BillyRyanShelton <35415026+BillyRyanShelton@users.noreply.github.com>
2018-07-23 14:41:51 -0500

    Create 038-getNthElement.js

---
+++ 038-getNthElement.js
@@ -0,0 +1,8 @@
+/*Given an array and an integer, “getNthElement” returns the element at the given integer, within the given array.*/
+
+function getNthElement(array, n) {
+  // your code here
+  /* START SOLUTION */
+  return array[n];
+  /* END SOLUTION */
+}
