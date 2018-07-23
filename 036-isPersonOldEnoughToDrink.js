e1fd103349c9e6204c0c6ba9e1a8de334f6247f3
BillyRyanShelton <35415026+BillyRyanShelton@users.noreply.github.com>
2018-07-23 14:39:37 -0500

    Create 036-isPersonOldEnoughToDrink.js

---
+++ 036-isPersonOldEnoughToDrink.js
@@ -0,0 +1,9 @@
+/*Given a “person” object, that contains an “age” property, “isPersonOldEnoughToDrink”
+returns whether the given person is old enough to drink.*/
+
+function isPersonOldEnoughToDrink(person) {
+  // your code here
+  /* START SOLUTION */
+  return person.age > 20;
+  /* END SOLUTION */
+}
