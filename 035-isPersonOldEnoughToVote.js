53d38400149452ce7c579570b62eada00cc13870
BillyRyanShelton <35415026+BillyRyanShelton@users.noreply.github.com>
2018-07-23 14:39:01 -0500

    Create 035-isPersonOldEnoughToVote.js

---
+++ 035-isPersonOldEnoughToVote.js
@@ -0,0 +1,9 @@
+/*Given a “person” object, that contains an “age” property, “isPersonOldEnoughToVote” 
+returns whether the given person is old enough to vote.*/
+
+function isPersonOldEnoughToVote(person) {
+  // your code here
+  /* START SOLUTION */
+  return person.age > 17;
+  /* END SOLUTION */
+}
