26fcb7f5150238b712582c68eb0dbc5e02509900
BillyRyanShelton <35415026+BillyRyanShelton@users.noreply.github.com>
2018-07-23 14:19:25 -0500

    Create 009-getFullName.js

---
+++ 009-getFullName.js
@@ -0,0 +1,10 @@
+/*Write a function called “getFullName”.
+Given a first and a last name, “getFullName” returns a single string with the given first and last names separated by a single space.*/
+
+
+function getFullName(firstName, lastName) {
+  // your code here
+  /* START SOLUTION */
+  return firstName + ' ' + lastName;
+  /* END SOLUTION */
+}
