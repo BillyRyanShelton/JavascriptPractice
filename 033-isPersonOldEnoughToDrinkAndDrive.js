c1dfeba33b10d4b6a55adf1513822483f5c51c07
BillyRyanShelton <35415026+BillyRyanShelton@users.noreply.github.com>
2018-07-23 14:37:28 -0500

    Create 033-isPersonOldEnoughToDrinkAndDrive.js

---
+++ 033-isPersonOldEnoughToDrinkAndDrive.js
@@ -0,0 +1,12 @@
+/*Write a function called “isPersonOldEnoughToDrinkAndDrive”.
+
+Given a “person” object, that contains an “age” property, 
+“isPersonOldEnoughToDrinkAndDrive” returns whether the given 
+person is old enough to legally drink and drive in the United States.*/
+
+function isPersonOldEnoughToDrinkAndDrive(person) {
+  // your code here
+  /* START SOLUTION */
+  return false;
+  /* END SOLUTION */
+}
